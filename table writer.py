e8ed327d92a8d3f9da9b3768abcf9c1d93c199a2
Niconikun <nicolassepulvedaestrada@gmail.com>
2025-01-21 18:34:25 -0300

    Update table writer.py

--- table writer.py
+++ table writer.py
@@ -1,6 +1,8 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
+
 
 def save_to_excel(data, filename):
     # Function to save extracted data to an Excel file
@@ -11,25 +13,57 @@ def save_to_excel(data, filename):
         ws.append(item)
     wb.save(filename)
 
-def main():
+
+if __name__ == '__main__':
     base_url = 'https://www.nanosats.eu/database'
     response = requests.get(base_url)
     html = response.text
     soup = BeautifulSoup(html, "lxml")
+    links = []
+    #print(soup.table)
+    for link in soup.table.find_all('a'):
+        links.append(link.get('href'))
 
-    #soup = BeautifulSoup(response.content, 'html.parser')
-    table = soup.find('table', {'class': 'table-wrapper nc'})  # Assuming table exists in the page
-    # Extract data from the table and return relevant links
-    headers = []
-    rows = []
-    for i, row in enumerate(table.find_all('tr')):
-        if i == 0:
-            headers = [el.text.strip() for el in row.find_all('th')]
-        else:
-            rows.append([el.text.strip() for el in row.find_all('td')])
+    links_filtered = [ x for x in links if '.html' in x ]
+    links_filtered = [ x for x in links_filtered if 'https://' not in x ]
+    #print(links_filtered)
+    dict = {}
 
-    save_to_excel(extracted_data, 'extracted_data.xlsx')
+    for sat_link in links_filtered:
+        base_url = 'https://www.nanosats.eu/' + sat_link
+        response_sat = requests.get(base_url)
+        html_sat = response_sat.text
+        soup_sat = BeautifulSoup(html_sat, "lxml")
+        table_sat = soup_sat.find('table', id='table-company')
+        print(table_sat)
 
-if __name__ == '__main__':
-    main()
+        for row in table_sat.find_all('tr'):
+
+            if row.find('th').get_text() == 'Spacecraft name' or row.find('th').get_text() == 'Spacecraft': #element in istitution exists or has another name
+                dict.update({'Spacecraft name': table_sat.find('td').get_text()})
+            
+            if row.find('th').get_text() == 'Launcher': #element in istitution exists or has another name
+                dict.update({'Launcher': table_sat.find('td').get_text()})
+            
+            if row.find('th').get_text() == 'Organisation': #element in istitution exists or has another name
+                dict.update({'Organisation': table_sat.find('td').get_text()})
+                
+            if row.find('th').get_text() == 'Institution': #element in istitution exists or has another name
+                dict.update({'Institution': table_sat.find('td').get_text()})
+
+            if row.find('th').get_text() == 'Entity type': #element in istitution exists or has another name
+                dict.update({'Entity type': table_sat.find('td').get_text()})
+
+            if row.find('th').get_text() == 'Manufacturer': #element in istitution exists or has another name
+                dict.update({'Manufacturer': table_sat.find('td').get_text()})
+
+            
+            
+            
+            
+            
+
+        
+    table_to_excel = pd.DataFrame(dict)
+    print(table_to_excel) 
 
